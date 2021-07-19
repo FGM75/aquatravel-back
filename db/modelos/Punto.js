f01c4bca4f75b47fb68ffec824fee6458436d10c
Michaelsp11 <michaelsalgadop11@gmail.com>
2021-07-19 14:13:39 +0200

    Schema de Punto

---
+++ db/modelos/Punto.js
@@ -0,0 +1,40 @@
+const { Schema, model } = require("mongoose");
+
+const PuntoSchema = new Schema({
+  nombre: {
+    type: String,
+    required: true,
+  },
+  provincia: {
+    type: String,
+    required: true,
+  },
+  comunidad: {
+    type: String,
+    required: true,
+  },
+  coordenadas: {
+    type: [Number],
+    validate: [
+      (coordenadas) => coordenadas.length === 2,
+      "Deben introducirse dos coordenadas",
+    ],
+    required: true,
+  },
+  servicios: {
+    type: Schema.Types.ObjectId,
+    ref: "Servicio",
+  },
+  tipoPunto: {
+    type: String,
+    required: true,
+  },
+  valoraciones: {
+    type: [Schema.Types.ObjectId],
+    ref: "Valoracion",
+  },
+});
+
+const Punto = model("Punto", PuntoSchema, "puntos");
+
+module.exports = Punto;
