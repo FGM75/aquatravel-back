2f544f82af9b4dd49b31302fc7153ebeaca11242
Michaelsp11 <michaelsalgadop11@gmail.com>
2021-07-19 13:39:05 +0200

    Servidor ya se levanta

--- servidor/index.js
+++ servidor/index.js
@@ -1,15 +1,12 @@
 const morgan = require("morgan");
 const express = require("express");
 const cors = require("cors");
-const app = require("./init"); 
+const app = require("./init");
 const { errorGeneral, error404 } = require("./errores");
 
+// app.use(cors());
+// app.use(morgan("dev"));
+// app.use(express.json());
 
-app.use(cors());
-app.use(morgan("dev"));
-app.use(express.json());
-
-
-
-app.use(errorGeneral);
-app.use(error404);
+// app.use(errorGeneral);
+// app.use(error404);
