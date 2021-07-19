0ec7be00660e64da0419508b71db05b531e7a03a
Michaelsp11 <michaelsalgadop11@gmail.com>
2021-07-19 20:28:27 +0200

    Schema de comentario y favorito renombrados + sangrado de archivos

---
+++ db/modelos/Favorito.js
@@ -0,0 +1,19 @@
+const { Schema, model } = require("mongoose");
+
+const favoritoSchema = new Schema(
+  {
+    id: {
+      type: String,
+      requiere: true,
+    },
+    creadoId: {
+      type: [Schema.Types.ObjectId],
+      ref: "Usuarios",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+const Favorito = model("Favorito", favoritoSchema, "favoritos");
+module.exports = Favorito;
--- db/modelos/Usuarios.js
+++ db/modelos/Usuarios.js
@@ -1,29 +1,29 @@
-const { Schema, model, SchemaType } = require("mongoose");
+const { Schema, model } = require("mongoose");
 
 const UsuarioShema = new Schema({
-    usuario: {
-        type: String,
-        unique: true,
-        required: true,
-    },
-    constrasenya: {
-        type: String,
-        minLength: 6,
-        required: true,
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    favoritos: {
-        type: [Schema.Types.ObjectId],
-        ref: "Punto",
-    },
-    comentarios: {
-        type: [Schema.Type.ObjectId],
-        ref: "Comentario"
-    },
+  usuario: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  constrasenya: {
+    type: String,
+    minLength: 6,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  favoritos: {
+    type: [Schema.Types.ObjectId],
+    ref: "Punto",
+  },
+  comentarios: {
+    type: [Schema.Type.ObjectId],
+    ref: "Comentario",
+  },
 });
 const Usuario = model("Usuario", UsuarioShema, "usuarios");
 module.exports = Usuario;
