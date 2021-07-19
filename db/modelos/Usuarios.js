505b1eb0a0dc235f6c95f6c09f7942a643032c0c
Felipe <fgonzalezm75@gmail.com>
2021-07-19 14:14:45 +0200

    schema usuario

---
+++ db/modelos/Usuarios.js
@@ -0,0 +1,31 @@
+const { Schema, model, SchemaType } = require("mongoose");
+
+const UsuarioShema = new Schema({
+    usuario: {
+        type: String,
+        unique: true,
+        required: true,
+    },
+    constrasenya: {
+        type: String,
+        minLength: 6,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    favoritos: {
+        type: [Schema.Types.ObjectId],
+        ref: "Punto",
+    },
+    comentarios: {
+        type: [Schema.Type.ObjectId],
+        ref: "Comentario"
+    },
+});
+const Usuario = model("Usuario", UsuarioShema, "usuarios");
+module.exports = {
+    Usuario,
+};
